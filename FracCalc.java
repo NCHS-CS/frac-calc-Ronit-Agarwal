a03b9341eedd8159a4dda1d5ba424842a6be9180
Ronit-Agarwal <2002599@apps.nsd.org>
2025-12-12 22:16:26 +0000

    test 3

--- FracCalc.java
+++ FracCalc.java
@@ -1,10 +1,13 @@
-// Student Name
-// Period X
+// Ronit Agarwal
+// Period 6
+// Mrs. Abuja
 // Fraction Calculator Project
 
 import java.util.*;
 
-// TODO: Description of what this program does goes here.
+// This program takes in two fractions and an operator, then performs the
+// appropriate calculation (addition, subtraction, multiplication, or division).
+// It simplifies the resulting fraction and outputs the final reduced form back to the user.
 public class FracCalc {
 
    // It is best if we have only one console object for input
@@ -44,8 +47,9 @@ public class FracCalc {
    // Prompt the user with a simple, "Enter: " and get the line of input.
    // Return the full line that the user typed in.
    public static String getInput() {
-      // TODO: Implement this method
-       return "quit";
+
+      System.out.print("Enter: ");
+      return console.nextLine();
 
    }
    
@@ -80,20 +84,231 @@ public class FracCalc {
    //        2 1/8
    //        2 1/4
    public static String processExpression(String input) {
-      // TODO: implement this method!
-    
-        return "reduced result of expression";
 
+      //Find where first space is in input
+      int firstSpace = input.indexOf(" ");
+      //Initialize second space variable
+      int secondSpace = -1;
+      
+      //Using first space, find where the second space is now
+      for (int i = firstSpace + 1; i < input.length(); i++) {
+        if (input.charAt(i) == ' ') {
+            secondSpace = i;
+            break;
+         }
+      }
+
+      //Find the operator and the second number and the first number
+      String op = extractOperator(input, firstSpace, secondSpace);
+      String second = extractSecondNumber(input, secondSpace);
+      String first = input.substring(0, firstSpace);
+
+      //Parse the first and second numbers into whole, numerator, denominator
+      int whole1 = parseWhole(first);
+      int num1 = parseNumerator(first);
+      int den1 = parseDenominator(first);
+
+      int whole2 = parseWhole(second);
+      int num2 = parseNumerator(second);
+      int den2 = parseDenominator(second); 
+
+      //Convert both numbers to improper fractions
+      num1 = convertToInproper(whole1, num1, den1);
+      num2 = convertToInproper(whole2, num2, den2);
+
+      //Find the common denominator and change numerator accordingly
+      int commonDen = den1 * den2;
+      int resultNum = num1 * den2 + num2 * den1;
+      int resultDen = commonDen;
+
+      //Finds what factor to reduce the answer by and reduces it
+      int gcdValue = gcd(resultNum, resultDen);
+      resultNum /= gcdValue;
+      resultDen /= gcdValue;
+
+      //Noramlizes the fraction so denominator is always positive
+      if (resultDen < 0) {
+         resultDen = -resultDen;
+         resultNum = -resultNum;
+      }
+
+      //Formats and returns the result
+      return formatResult(resultNum, resultDen);
    }
+
+   //Uses .contains, learned by https://www.w3schools.com/java/ref_string_contains.asp
+   //Parse whole number from input String
+   //input - full operand String
+   //Return whole number as an integer
+   public static int parseWhole(String input) {
+      if (input.contains("_")) {
+         return extractWholeFromMixed(input);
+      } else if (input.contains("/")) {
+         return 0;
+      } else {
+         return extractWholeFromNumber(input);
+      }
+   }
+
+   //Parse numerator from input String
+   //input - full operand String
+   //Return numerator as an integer
+   public static int parseNumerator(String input) {
+      if (input.contains("_")) {
+         return extractNumeratorFromMixed(input);
+      } else if (input.contains("/")) {
+         return extractNumeratorFromFraction(input);
+      } else {
+         return 0;
+      }
+   }
+
+   //Parse denominator from input String
+   //input - full operand String
+   //Return denominator as an integer
+   public static int parseDenominator(String input) {
+      if (input.contains("_")) {
+         return extractDenominatorFromMixed(input);
+      } else if (input.contains("/")) {
+         return extractDenominatorFromFraction(input);
+      } else {
+         return 1;
+      }
+   }
+
+   //Convert mixed number to improper fraction
+   //whole - whole number part of mixed number
+   //num - numerator of mixed number
+   //den - denominator of mixed number
+   //Return numerator of improper fraction
+   public static int convertToInproper(int whole, int num, int den) {
+      if (whole < 0) {
+         return whole * den - num;
+      } else {
+         return whole * den + num;
+      }
+   }  
+
+   //Format the result into a String
+   //num - numerator of result
+   //den - denominator of result
+   //Return formatted result as a String
+   public static String formatResult(int num, int den) {
+      if (num == 0){
+         return "0";
+      }
+
+      int whole = num / den;
+      int remainder = num % den;
+
+      if (remainder < 0) {
+         remainder = -remainder;
+      }
+
+      if (remainder == 0) {
+         return "" + whole;
+      }
+
+      if (whole == 0) {
+         if (num < 0) {
+            return "-" + remainder + "/" + den;
+         } else {
+            return remainder + "/" + den;
+         }
+      }
+      return whole + " " + remainder + "/" + den;
+   }
+
+
+   //Extract operator from String
+   //input - full expression String
+   //firstSpace - index of first Space Character
+   //secondSpace - index of second Space Character
+   //Return operator as a String
+   public static String extractOperator(String input, int firstSpace, int secondSpace){
+      return input.substring(firstSpace + 1, secondSpace);
+   }
+
+
+   //Extract second operand from String
+   //input - full expression String
+   //secondSpace - index of second Space Character
+   //Return second operand as a String
+   public static String extractSecondNumber(String input, int secondSpace){
+      return input.substring(secondSpace + 1);
+   }
+
+   //Extract whole number from mixed number String
+   //second - second operand String in mixed number format
+   //Return whole number part as an integer
+   public static int extractWholeFromMixed(String second){
+      int underscore = second.indexOf("_");
+      return Integer.parseInt(second.substring(0, underscore));
+   }
+
+   //Extract numerator from mixed number String
+   //second - second operand String in mixed number format
+   //Return numerator part as an integer
+   public static int extractNumeratorFromMixed(String second){
+      int underscore = second.indexOf("_");
+      int slash = second.indexOf("/");
+      return Integer.parseInt(second.substring(underscore + 1, slash));
+   }
+
+   //Extract denominator from mixed number String
+   //second - second operand String in mixed number format
+   //Return denominator part as an integer
+   public static int extractDenominatorFromMixed(String second){
+      int slash = second.indexOf("/");
+      return Integer.parseInt(second.substring(slash + 1));
+   }
+
+   //Extract numerator from simple fraction String
+   //second - second operand String in fraction format
+   //Return numerator as an integer
+   public static int extractNumeratorFromFraction(String second){
+      int slash = second.indexOf("/");
+      return Integer.parseInt(second.substring(0, slash));
+   }
+
+   //Extract denominator from simple fraction String
+   //second - second operand String in fraction format
+   //Return denominator as an integer
+   public static int extractDenominatorFromFraction(String second){
+      int slash = second.indexOf("/");
+      return Integer.parseInt(second.substring(slash + 1));
+   }
+
+   //Extract whole number from whole number String
+   //second - second operand String as a whole number
+   //Return whole number as an integer
+   public static int extractWholeFromNumber(String second){
+      return Integer.parseInt(second);
+   }
+
+   // Calculate the greatest common divisor of two integers
+   // using the Euclidean algorithm https://www.geeksforgeeks.org/dsa/euclidean-algorithms-basic-and-extended/
+   //a - first integer
+   //b - second integer
+   //Return greatest common divisor of a and b
+   public static int gcd(int a, int b) {
+      if (b == 0) {
+         return Math.abs(a);
+      }
+      return gcd(b, a % b);
+   }
+
+   
    
    // Returns a string that is helpful to the user about how
    // to use the program. These are instructions to the user.
    public static String provideHelp() {
-      // TODO: Update this help text!
-     
-      String help = "You must change this text.\n";
-      help += "Students, you need to provide actual helpful text here!";
-      
+      // Prints out help text
+      String help = "Fraction Calculator Help:\n";
+      help += "Enter mathematical expressions with two numbers and an operator (+, -, *, /)\n";
+      help += "Make sure to put underscores between whole numbers and fractions for mixed numbers.\n";
+      help += "Examples: 1/2 + 1/4, 3 * 2_1/2, 4/5 - 1/3\n";
+      help += "Commands: 'help' - show this help, 'quit' - exit program";
       return help;
    }
 }
